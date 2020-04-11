1794896d4a141241cf9361505c433b6ec490f732
Sujit More <sujitmore21@gmail.com>
2020-04-12 00:51:32 +0530

    Product Details Activity =====Done

--- app/src/main/java/com/example/e_shop/ProductDetailsActivity.java
+++ app/src/main/java/com/example/e_shop/ProductDetailsActivity.java
@@ -2,10 +2,28 @@ package com.example.e_shop;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProductDetailsActivity extends AppCompatActivity {
+
+    private ViewPager productImageViewPager;
+    private TabLayout viewpagerIndicator;
+
+    private static boolean ALLREDY_ADDED_TO_WISHLIST = false;
+    private FloatingActionButton addToWishlistBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +34,60 @@ public class ProductDetailsActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        productImageViewPager = findViewById(R.id.product_images_viewpager);
+        viewpagerIndicator = findViewById(R.id.viewpager_indicater);
+        addToWishlistBtn = findViewById(R.id.add_to_wishlist_btn);
+
+        List<Integer> productImages = new ArrayList<>();
+        productImages.add(R.drawable.profile_img);
+        productImages.add(R.drawable.forgotton_pw);
+        productImages.add(R.drawable.profile_img);
+        productImages.add(R.drawable.forgotton_pw);
+
+        ProductImagesAdapter productImagesAdapter = new ProductImagesAdapter(productImages);
+        productImageViewPager.setAdapter(productImagesAdapter);
+
+        viewpagerIndicator.setupWithViewPager(productImageViewPager,true);
+
+        addToWishlistBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ALLREDY_ADDED_TO_WISHLIST){
+                    ALLREDY_ADDED_TO_WISHLIST = false;
+                    addToWishlistBtn.setSupportBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#9e9e9e")));
+                } else {
+                    ALLREDY_ADDED_TO_WISHLIST = true;
+                    addToWishlistBtn.setSupportImageTintList(getResources().getColorStateList(R.color.colorPrimary));
+                }
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.search_and_card_icon, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == android.R.id.home) {
+            return true;
+        } else if (id == R.id.main_search_icon) {
+            //todo: search
+            return true;
+        } else if (id == R.id.main_cart_icon) {
+            //todo: card
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
